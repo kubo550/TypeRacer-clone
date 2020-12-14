dca1a6082bcb16249b6488a2a0dbde67db6c8a92
kubo550 <43968748+kubo550@users.noreply.github.com>
2020-12-14 22:20:02 +0100

    Info Div

---
+++ src/components/Info/Info.tsx
@@ -0,0 +1,11 @@
+const Info = () => (
+  <div className='mb-4'>
+    <h4 className='text-bold'> Practice Racetrack </h4>
+    <p>
+      You are in a single-player race. Click <a href='#!'>here</a> if you'd like some
+      competition.
+    </p>
+  </div>
+);
+
+export default Info;
