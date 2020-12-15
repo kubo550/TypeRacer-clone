009f361351b7266589582216997efac87e0fa06d
kubo550 <43968748+kubo550@users.noreply.github.com>
2020-12-15 01:03:52 +0100

    Player

--- src/components/Race/Player/Player.tsx
+++ src/components/Race/Player/Player.tsx
@@ -1,5 +1,21 @@
 import { FC } from "react";
 import carImg from "../../../assets/car.svg";
+import styled from "styled-components";
+
+const Road = styled.div`
+  width: 100%;
+  height: 4px;
+  background-color: white;
+  padding-right: 40px;
+  ::after {
+    content: "";
+    position: absolute;
+    width: 46px;
+    right: 15px;
+    height: 4px;
+    background-color: #6e40c9;
+  }
+`;
 
 interface playerProps {
   distance: number;
@@ -7,14 +23,17 @@ interface playerProps {
 
 const Player: FC<playerProps> = ({ distance }) => {
   return (
-    <img
-      src={carImg}
-      width='58'
-      height='24'
-      alt='player car'
-      style={{ marginLeft: `${distance - 7}%` }}
-      title='Powerty'
-    />
+    <>
+      <img
+        src={carImg}
+        width='58'
+        height='24'
+        alt='player car'
+        style={{ marginLeft: `${distance * 100 - 7}%` }}
+        title='Powerty'
+      />
+      <Road />
+    </>
   );
 };
 
