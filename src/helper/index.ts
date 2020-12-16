45979cb638d611d8f0a6ca3c2546e87cb1f086a3
kubo550 <43968748+kubo550@users.noreply.github.com>
2020-12-16 22:08:40 +0100

    Count Stats Function

---
+++ src/helper/index.ts
@@ -0,0 +1,18 @@
+export const countStats = (arr1: number[]) => {
+    if (!arr1.length) return [0, 0, 0, 0];
+    const arr = [...arr1];
+
+    const getAvg = (numArr: number[]) =>
+        numArr.reduce((prev, curr, idx, arr) => {
+            if (idx === arr.length - 1) {
+                return (prev + curr) / arr.length;
+            }
+            return prev + curr;
+        }, 0);
+
+    const max = arr.reduce((prev, curr) => (curr > prev ? curr : prev), 0);
+    const last = arr[arr.length - 1];
+    const avg = getAvg(arr);
+    const avgLast10 = getAvg([...arr].slice(-10));
+    return [max, last, avg, avgLast10];
+};
