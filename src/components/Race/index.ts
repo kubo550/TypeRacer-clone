57f7a0417bd0c3e7dc73a6b2b662d917db1d7efd
kubo550 <43968748+kubo550@users.noreply.github.com>
2020-12-16 22:13:24 +0100

    Eksports Race Components

---
+++ src/components/Race/index.ts
@@ -0,0 +1,6 @@
+import MainInput from "./MainInput/MainInput";
+import Player from "./Player/Player";
+import SingleWord from "./SingleWord/SingleWord";
+import Timmer from "./Timmer/Timmer";
+
+export { MainInput, Player, SingleWord, Timmer }
