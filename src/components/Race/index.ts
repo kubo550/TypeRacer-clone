ae1c15a7cecebfce18913a441ec50dd0fef33142
kubo550 <43968748+kubo550@users.noreply.github.com>
2020-12-17 17:11:50 +0100

    Import EndRaceInfo Component

--- src/components/Race/index.ts
+++ src/components/Race/index.ts
@@ -2,5 +2,6 @@ import MainInput from "./MainInput/MainInput";
 import Player from "./Player/Player";
 import SingleWord from "./SingleWord/SingleWord";
 import Timmer from "./Timmer/Timmer";
+import EndRaceInfo from "./EndRaceInfo/EndRaceInfo";
 
-export { MainInput, Player, SingleWord, Timmer }
+export { MainInput, Player, SingleWord, Timmer, EndRaceInfo }
