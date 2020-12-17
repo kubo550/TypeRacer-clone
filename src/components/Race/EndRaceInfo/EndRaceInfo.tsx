3e9e66d90175785e1a2e76bc9ab5414df8e76efd
kubo550 <43968748+kubo550@users.noreply.github.com>
2020-12-17 17:13:08 +0100

    Update Layout App

--- src/components/Race/EndRaceInfo/EndRaceInfo.tsx
+++ src/components/Race/EndRaceInfo/EndRaceInfo.tsx
@@ -1,57 +1,45 @@
-import styled from "styled-components";
-import { BorderedDiv } from "../Race";
-interface EndRaceProps {
-  restartGame: (text: string) => void;
-  currentText: string;
-  nextText: string;
-}
+import { BorderedDiv, ButtonsContainer } from "../layout";
+import { EndRaceProps } from "../types";
 
-const ButtonsContainer = styled.div`
-  display: flex;
-  justify-content: space-evenly;
-`;
-
-const EndRaceInfo: React.FC<EndRaceProps> = ({ restartGame, currentText, nextText }) => {
-  return (
-    <BorderedDiv borderColor='#f0883e' className='mb-4'>
-      <h4 className='h3 mb-4'>
-        Cytowany Fragment: <span className='ml-3'>Pismo Święte </span>
-      </h4>
-      <div className='row'>
-        <div className='col-md-6'>
-          <img
-            src='https://hurtownianotus.pl/wp-content/uploads/2017/10/czerw.png'
-            width='140'
-            className='img ml-5'
-            alt='cytowany fragment img'
-          />
-        </div>
-        <div className='col-md-6'>
-          <ButtonsContainer className='mt-2 mb-4'>
-            <button
-              className='btn btn-outline-pourple'
-              onClick={() => restartGame(currentText)}
-            >
-              Try Again 🔙
-            </button>
-            <button
-              className='btn btn-outline-orange'
-              onClick={() => restartGame(nextText)}
-            >
-              Next Race 🚓
-            </button>
-          </ButtonsContainer>
-          <p className='mt-3'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quis ipsa
-            distinctio earum vel! Minima nostrum dicta.
-          </p>
-          <p>
-            Dowiedz się więcej <a href='#!'>tutaj</a>.
-          </p>
-        </div>
+const EndRaceInfo: React.FC<EndRaceProps> = ({ restartGame, currentText, nextText }) => (
+  <BorderedDiv borderColor='#f0883e' className='mb-4'>
+    <h4 className='h3 mb-4'>
+      Cytowany Fragment: <span className='ml-3'>Pismo Święte </span>
+    </h4>
+    <div className='row'>
+      <div className='col-md-6'>
+        <img
+          src='https://hurtownianotus.pl/wp-content/uploads/2017/10/czerw.png'
+          width='140'
+          className='img ml-5'
+          alt='cytowany fragment img'
+        />
       </div>
-    </BorderedDiv>
-  );
-};
+      <div className='col-md-6'>
+        <ButtonsContainer className='mt-2 mb-4'>
+          <button
+            className='btn btn-outline-pourple'
+            onClick={() => restartGame(currentText)}
+          >
+            Try Again 🔙
+          </button>
+          <button
+            className='btn btn-outline-orange'
+            onClick={() => restartGame(nextText)}
+          >
+            Next Race 🚓
+          </button>
+        </ButtonsContainer>
+        <p className='mt-3'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quis ipsa
+          distinctio earum vel! Minima nostrum dicta.
+        </p>
+        <p>
+          Dowiedz się więcej <a href='#!'>tutaj</a>.
+        </p>
+      </div>
+    </div>
+  </BorderedDiv>
+);
 
 export default EndRaceInfo;
--- src/components/Stats/Stats.tsx
+++ src/components/Stats/Stats.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import styled from "styled-components";
-import { BorderedDiv } from "../Race/Race";
+import { BorderedDiv } from "../Race/layout";
 import { countStats } from "../../helper";
 
 const Grid = styled.div`
@@ -18,28 +18,28 @@ const Stats: FC<statsProps> = ({ wordsPerMinute }) => {
   const [best, last, avg, avgLast10] = countStats(wordsPerMinute);
 
   return (
-    <BorderedDiv className='mb-5' borderColor='#388BFC'>
+    <BorderedDiv className='mt-5 mb-4' borderColor='#388BFC'>
       <h4>Your Stats</h4>
       <Grid>
-        <div>
+        <div className='my-1'>
           Avg last 10 WPM:
           <div>
             <b> {avgLast10.toFixed(1)} </b>
           </div>
         </div>
-        <div>
+        <div className='my-1'>
           Avg All WPM:
           <div>
             <b>{avg.toFixed(1)}</b>
           </div>
         </div>
-        <div>
+        <div className='my-1'>
           Last WPM:
           <div>
             <b> {last.toFixed(1)} </b>
           </div>
         </div>
-        <div>
+        <div className='my-1'>
           Best WPM:
           <div>
             <b>{best.toFixed(1)}</b>
